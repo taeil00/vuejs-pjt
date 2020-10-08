eb9f0cbfa6298b2ac9732993a689dd82169f7d07
TAEIL JUNG <taeil@naver.com>
2020-10-09 00:30:57 +0900

    스토어 속성 모듈화

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList} from '../api/index.js';
+
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
@@ -16,44 +18,7 @@ export const store =  new Vuex.Store({
             return state.ask;
         }
     },
-    mutations:{
-        SET_NEWS(state, news){
-            state.news = news;
-        },
-        SET_ASK(state, ask){
-            state.ask = ask;
-        },
-        SET_JOBS(state, jobs){
-            state.jobs = jobs;
-        }
-    },
-    actions: {
-        FETCH_NEWS(context){
-            fetchNewsList()
-            .then(response => {
-                context.commit('SET_NEWS', response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_ASK( {commit}){
-            fetchAskList()
-            .then( ({ data}) => {
-                commit('SET_ASK', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_JOBS(context){
-            fetchJobsList()
-            .then(response => {
-                context.commit('SET_JOBS', response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        }
-    }
+    mutations,
+    actions,
+    
 });
